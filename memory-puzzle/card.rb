9693a2afadebf33abdb7cee1382cb8545c98c419
ncioffi1 <imnickcioffi@gmail.com>
2023-10-30 16:15:04 -0400

    first commit

---
+++ memory-puzzle/card.rb
@@ -0,0 +1,7 @@
+# git status
+    # get git status of directory.
+# git init
+    # create git repository at directory point.
+
+# git commit -m "message"
+    # commits your git.  always have a message.  nothing too fancy.
