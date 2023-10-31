c72c2b3a57316e4a77b689225ffe58be6193b9a1
ncioffi1 <imnickcioffi@gmail.com>
2023-10-31 12:05:23 -0400

    changed

---
+++ github_commands.rb
@@ -0,0 +1,20 @@
+# git status
+    # get git status of directory.
+# git init
+    # create git repository at directory point.
+
+# git commit -m "message"
+    # commits your git.  always have a message.  nothing too fancy.
+
+# git add -A
+    # adds all stages.  can also do git add .
+
+# cloning
+# https://docs.github.com/en/repositories/creating-and-managing-repositories/duplicating-a-repository
+
+class Whatever
+    def initialize(name, size)
+        @name = name
+        @size = size
+    end
+end
